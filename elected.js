33be6fa5814b78e0566f4f080d4d9c14ca873094
JD Martinez <focus4ursoul@gmail.com>
2020-10-01 00:18:24 -0700

    add filter results: federal, state, local & add test button to input search

--- elected.js
+++ elected.js
@@ -1,22 +1,53 @@
 
-var searchAdd = "994 Henderson Ave, Sunnyvale CA 94086";
 
-var queryURL = "https://www.googleapis.com/civicinfo/v2/representatives?address=" + searchAdd + "&key=AIzaSyB9_EXjp94LjHiu63YgUiOSNUOJaAe4cII"
+var apiKey = "AIzaSyB9_EXjp94LjHiu63YgUiOSNUOJaAe4cII"
+var searchAdd = "New York, NY";
+var office;
+var elected;
 
-$.ajax({
-    url: queryURL,
-    type: "GET"
-}).then(function (r) {
-    console.log(r.offices);
+function getEOinfo() {
 
-    for (i = 0; i < r.offices.length; i++) {
+    var queryURL = "https://www.googleapis.com/civicinfo/v2/representatives?address=" + searchAdd + "&key=" + apiKey;
 
+    $.ajax({
+        url: queryURL,
+        type: "GET"
+    }).then(function (r) {
+        office = r.offices;
+        elected = r.officials;
 
-        console.log(r.offices[i].name);
+        console.log(r);
 
-        for(j = 0; j < r.offices[i].officialIndices.length; j++) {
-            console.log(r.officials[r.offices[i].officialIndices[j]].name);
+        var natLVL = false;
+        var stateLVL = false;
+        var localLVL = true;
+        var state = r.normalizedInput.state;
+
+        for (i = 0; i < r.offices.length; i++) {
+
+            if (natLVL === true && (r.offices[i].name.includes("President") || r.offices[i].name.includes("U.S."))) {
+                printEO();
+            } else if (stateLVL === true && (r.offices[i].name.includes(state) || r.offices[i].name.includes("Governor"))) {
+                printEO();
+            } else if (localLVL === true && !r.offices[i].name.includes(state) && !r.offices[i].name.includes("Governor") && !r.offices[i].name.includes("U.S.") && !r.offices[i].name.includes("President")) {
+                printEO();
+            }
         };
-    };
+    });
+};
 
+function printEO() {
+    console.log(office[i].name);
+
+    for (j = 0; j < office[i].officialIndices.length; j++) {
+        console.log(elected[office[i].officialIndices[j]].name);
+    };
+};
+
+$("#addSearchBtn").on("click", function () {
+    var ipnut = $("#addSearchInput");
+
+    searchAdd = ipnut.val();
+    getEOinfo();
+    ipnut.val("");
 });
