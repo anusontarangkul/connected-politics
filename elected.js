25e5da499219a58a43e888e03c2b914f5527a1d9
JD Martinez <focus4ursoul@gmail.com>
2020-09-30 22:14:38 -0700

    add func to print elected officials on to console.log

--- elected.js
+++ elected.js
@@ -1,46 +1,22 @@
 
-var searchAdd = "141 E 56th St, New York, NY 10022";
+var searchAdd = "994 Henderson Ave, Sunnyvale CA 94086";
 
 var queryURL = "https://www.googleapis.com/civicinfo/v2/representatives?address=" + searchAdd + "&key=AIzaSyB9_EXjp94LjHiu63YgUiOSNUOJaAe4cII"
 
 $.ajax({
     url: queryURL,
     type: "GET"
-}).then(function(r){
+}).then(function (r) {
+    console.log(r.offices);
 
-    console.log(r.offices.length);
-
-    // for(i = 0; i < offices.length )
-
-
-    console.log(r);
+    for (i = 0; i < r.offices.length; i++) {
 
 
-    // //President print
-    // console.log(r.offices[0].name + ": " + r.officials[r.offices[0].officialIndices[0]].name);
+        console.log(r.offices[i].name);
 
-    // //V. President print
-    // console.log(r.offices[1].name + ": " + r.officials[r.offices[1].officialIndices[0]].name);
-
-    // //U.S Senator print
-    // // console.log(r.offices.name[2].name + ": " + printOfficials());
-    // console.log(r.offices[2].name + ": " + r.officials[r.offices[2].officialIndices[0]].name);
-
-    
-
-    // var office = response.offices[8];
-    // for(i = 0; i < office.officialIndices.length; i++){
-    //     console.log(response.officials[office.officialIndices[i]]);
-    // };
-
-
+        for(j = 0; j < r.offices[i].officialIndices.length; j++) {
+            console.log(r.officials[r.offices[i].officialIndices[j]].name);
+        };
+    };
 
 });
-
-//have var office = be an iteration that runs through all of the office names
-
-// function printOfficials(office){
-
-//     var addToString = "";
-//     office 
-// };
